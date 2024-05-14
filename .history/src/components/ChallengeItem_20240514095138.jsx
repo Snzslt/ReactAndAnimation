b04758ec21ebf83e9b3c5a23b3bfc905eba0a26f
Sanaz Soltani <74157401+Snzslt@users.noreply.github.com>
2024-05-14 09:54:26 -0400

    animations with animations

--- .history/src/components/ChallengeItem_20240514095138.jsx
+++ .history/src/components/ChallengeItem_20240514095138.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import {motion, AnimatePresence} from 'framer-motion';
+import {motion} from 'framer-motion';
 
 import { ChallengesContext } from '../store/challenges-context.jsx';
 
